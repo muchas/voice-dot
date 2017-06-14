8e9142e1a3892ad1738beec16d7b68f3efb1ac5b
Sławomir Mucha <mucha.slawomir@gmail.com>
2017-06-14 14:48:21 +0200

    WIN

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import Web.Spock
 import Web.Spock.Config
 
+import Control.Monad.Trans
 import Data.Aeson hiding (json)
 import  Data.Monoid ((<>))
 import  Data.Text (Text, pack)
@@ -18,7 +19,7 @@ data Transcript = Transcript
   } deriving (Generic, Show)
 
 data NewReplaceCommand = NewReplaceCommand
-  { pattern :: String,
+  { pat :: String,
     replacement :: String
   } deriving (Generic, Show)
 
@@ -28,26 +29,28 @@ instance FromJSON Transcript
 instance ToJSON NewReplaceCommand
 instance FromJSON NewReplaceCommand
 
--- data AppState = DummyAppState (IORef [Command])
-type Api = SpockM () () () ()
-type ApiAction a = SpockAction () () () a
+data AppState = DummyAppState (IORef [Command])
+type Api = SpockM () () AppState ()
+type ApiAction a = SpockAction () () AppState a
 
 
 main :: IO ()
 main =
     do ref <- newIORef predefinedCommands
-       spockCfg <- defaultSpockCfg () PCNoDatabase ()
+       spockCfg <- defaultSpockCfg () PCNoDatabase (DummyAppState ref)
        runSpock 8080 (spock spockCfg app)
 
 app :: Api
 app =
     do
        post root $ do
-        --  (DummyAppState ref) <- getState
-        --  ioCmds <- readIORef ref
-        --  commands <- ioCmds
+         (DummyAppState ref) <- getState
+         commands <- liftIO $ atomicModifyIORef' ref $ \commands -> (commands, commands)
          theTranscript <- jsonBody' :: ApiAction Transcript
-         text (pack $ (processTranscript predefinedCommands) (t theTranscript))
+         text (pack $ (processTranscript commands) (t theTranscript))
        post "add" $ do
          theCommand <- jsonBody' :: ApiAction NewReplaceCommand
+         (DummyAppState ref) <- getState
+         commands <- liftIO $ atomicModifyIORef' ref $ \commands -> (addCustomReplaceCommand commands (pat theCommand, replacement theCommand),
+                                                                     addCustomReplaceCommand commands (pat theCommand, replacement theCommand))
          text ("ok")
